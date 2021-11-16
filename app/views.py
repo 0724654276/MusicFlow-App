83300b1d3829ab08c54dc9136de49759f07560e9
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-11-16 11:12:26 +0300

    update file

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('index.html')
+@app.route('/music/<int:music_id>')
+def music(music_id):
+
+    '''
+    View music page function that returns the music details page and its data
+    '''
+    return render_template('music.html',id = music_id)
